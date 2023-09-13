d699c1d304fbba85c7ad718f0cf82b5905c4f250
Khant Lin Nyan <khantlinnyan011027@gmail.com>
2023-09-13 18:29:56 +0630

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const box = document.querySelector('.box')
+box.innerHTML('HI')
+
+const arrow = () => {
+    console.log("Hello world my fris")
+}
+
+function arrow(e) {
+    console.log(`HEllO ${e}`)
+}
+
+const Home = () => {
+    return (
+        <div>Hello</div>
+    )
+}
